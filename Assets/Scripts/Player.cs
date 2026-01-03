3223fe2618f95743655c3e950fd4f85c549313de
LamNV290304 <nguyenvietlam290304@gmail.com>
2026-01-04 02:25:04 +0700

    Repeat Background

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,29 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+
+    private int spritesIndex = 0;
     private Vector3 playerPosition;
     public float gravaity = -9.81f;
     public float jumpHeight = 5.0f;
 
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
     private void Update()
     {
         bool pressed =
-    (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame) ||
-    (Touchscreen.current != null &&
-     Touchscreen.current.primaryTouch.press.wasPressedThisFrame);
+            (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame) ||
+            (Touchscreen.current != null &&
+             Touchscreen.current.primaryTouch.press.wasPressedThisFrame);
 
         if (pressed)
         {
@@ -22,5 +35,16 @@ public class Player : MonoBehaviour
         playerPosition.y += gravaity * Time.deltaTime;
 
         transform.position += playerPosition * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        spritesIndex++;
+        if (spritesIndex >= sprites.Length)
+        {
+            spritesIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spritesIndex];
     }
 }
