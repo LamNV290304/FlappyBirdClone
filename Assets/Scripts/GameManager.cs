6f917cb61d49200573098550d84cb27dd133f63f
LamNV - Dev <nguyenvietlam290304@gmail.com>
2026-01-06 14:15:25 +0700

    UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
-    private int score = 0;
+    public Text scoreText;
+
+    public GameObject playButton;
+    public GameObject gameOverText;
+
+    private int score;
+
+    public void Awake()
+    {
+        score = 0;
+    }
+
+    public void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void StartGame()
+    {
+        score = 0;
+        playButton.SetActive(false);
+        gameOverText.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+        playButton.SetActive(true);
+    }
 
     public void GameOver()
     {
